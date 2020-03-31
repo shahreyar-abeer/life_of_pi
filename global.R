1dd61c9c5378dbd153dad7f00d2460ed2be9ee7f
Zauad <shahreyar.abeer@gmail.com>
2020-03-31 11:45:16 +0600

    tweaking plot aesthetics

--- global.R
+++ global.R
@@ -1,11 +1,13 @@
 
 
+
 library(gifski)
 
 the_data = function(n) {
-  x0 = 0  # origi
+  x0 = 0  # origin
   y0 = 0  # origin
   
+  set.seed(n)
   x = runif(n, -1, 1)
   y = runif(n, -1, 1)
   
@@ -29,14 +31,14 @@ make_anim1 = function(the_data) {
   
   n = nrow(the_data)
   
-  c1 = ifelse(n == 10000, "#DA291C", "#447099")  # color inside
-  c2 = ifelse(n == 10000, "#006747", "#515960")
+  c1 = ifelse(n == 100001, "#DA291C", "#393f47")  # color inside
+  c2 = ifelse(n == 100001, "#006747", "#88837d")
   
   p1 = ggplot() +
     geom_circle(aes(x0 = 0, y0 = 0, r = 1), color = "#515960") +
     geom_rect(aes(xmin = -1,ymin = -1, xmax = 1, ymax = 1),
               alpha = 0, color = "#515960") +
-    geom_point(aes(x = x, y = y, group = seq_along(id), color = point), size = .4, data = the_data) +
+    geom_point(aes(x = x, y = y, group = seq_along(id), color = point), size = .7, data = the_data) +
     hrbrthemes::theme_ft_rc() +
     theme(plot.background = element_rect(fill = "#272b30"),
           panel.background = element_rect(fill = "#272b30", color = "#272b30"),
@@ -47,7 +49,7 @@ make_anim1 = function(the_data) {
     ) +
     transition_reveal(along = id)
   
-  anim1 = animate(p1, nframes = 10, fps = 1, duration = 10,
+  anim1 = animate(p1, nframes = 30, fps = 2,
                   width = 680, height = 600, res = 120,
                   renderer = gifski_renderer(loop = F))
   
@@ -61,18 +63,23 @@ make_anim2 = function(the_data) {
     hrbrthemes::theme_ft_rc() +
     ylim(0, 6) +
     theme(plot.background = element_rect(fill = "#272b30"),
-          panel.background = element_rect(fill = "#272b30", color = "#272b30")) +
+          panel.background = element_rect(fill = "#272b30", color = "#272b30"),
+          plot.title = element_text(color = "#88837d")) +
     labs(
-      title = "pie_hat: {the_data$pi_hat[frame_along]}",
+      title = "estimated pi: {the_data$pi_hat[frame_along]}",
       caption = "The simulation in real time.
       Guess which line shows the original value!"
     ) +
     xlab("# of points") +
     transition_reveal(along = id)
   
-  anim2 = animate(p2, nframes = 10, fps = 1, duration = 10,
+  anim2 = animate(p2, nframes = 30, fps = 2,
                   width = 600, height = 500, res = 120,
                   renderer = gifski_renderer(loop = F))
   
   return(anim2)
 }
+
+
+## 
+ns = c(100, 1000, 10000, 100000, 100001)
